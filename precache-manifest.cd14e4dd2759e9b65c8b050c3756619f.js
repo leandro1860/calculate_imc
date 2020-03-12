0b6b2ba1add59da05b8f5419d6f5b2fb046bfa7f
Leandro <leandroemanuelpaez2010@hotmail.com>
2020-03-12 12:18:31 -0300

    Updates

---
+++ precache-manifest.cd14e4dd2759e9b65c8b050c3756619f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21e821d4dddb3f0221fab043de4541f0",
+    "url": "/calculate_imc/index.html"
+  },
+  {
+    "revision": "b50fb4e1da66c3b34af5",
+    "url": "/calculate_imc/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "adb377abe040dfcb49d0",
+    "url": "/calculate_imc/static/css/main.280fe502.chunk.css"
+  },
+  {
+    "revision": "b50fb4e1da66c3b34af5",
+    "url": "/calculate_imc/static/js/2.f6638595.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/calculate_imc/static/js/2.f6638595.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adb377abe040dfcb49d0",
+    "url": "/calculate_imc/static/js/main.4c20c5e1.chunk.js"
+  },
+  {
+    "revision": "1cf4622db788ac4367af",
+    "url": "/calculate_imc/static/js/runtime-main.6095f077.js"
+  }
+]);
